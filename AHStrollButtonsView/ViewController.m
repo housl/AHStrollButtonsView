132dc7d7570c5d772bb480e032aa799d92151766
housl <745824020@qq.com>
2015-07-22 13:33:09 +0800

    contentInset

--- AHStrollButtonsView/ViewController.m
+++ AHStrollButtonsView/ViewController.m
@@ -9,7 +9,7 @@
 #import "ViewController.h"
 #import "AHScrollButtonsView.h"
 
-@interface ViewController ()
+@interface ViewController ()<UIScrollViewDelegate>
 
 @end
 
@@ -21,15 +21,35 @@
     float width = self.view.frame.size.width;
     AHScrollButtonsView *sbView = [[AHScrollButtonsView alloc] initWithFrame:CGRectMake(0, 100, width, 40)];
     sbView.backgroundColor = [UIColor grayColor];
+    sbView.delegate = self;
     [self.view addSubview:sbView];
     
+    UIEdgeInsets inset = sbView.contentInset;
+    inset.left = 100;
+    
+    CGPoint offset = sbView.contentOffset;
+    offset.x = -50;
+    sbView.contentOffset = offset;
+    sbView.contentInset = inset;
+    
     [sbView resetButtonTitles:@[@"外观\n(200)",@"内饰",@"网友",@"座椅",@"挂饰",@"其他",@"评测",@"图解"] buttonWidth:60.];
-    [sbView resetSelectedIndex:4];
+//    [sbView resetSelectedIndex:4];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView
+{
+    UIEdgeInsets inset =  scrollView.contentInset;
+    NSLog(@"%@",NSStringFromUIEdgeInsets(inset));
+    
+    CGPoint point = scrollView.contentOffset;
+    NSLog(@"%@",NSStringFromCGPoint(point));
+    
 }
 
 @end
